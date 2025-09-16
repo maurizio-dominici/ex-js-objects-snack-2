01702f5624f5c9eacaa920dec5b1966c2690551d
maurizio-dominici <mauriziodominici4@gmail.com>
2025-09-16 15:48:58 +0200

    add bonus

--- main.js
+++ main.js
@@ -72,3 +72,97 @@
 // Qual è il metodo migliore per clonare l’oggetto chef, e perché? // ?  Il metodo migliore è quello per shallow copy (...) perche cosi riesco a tener traccia anche della funzione
 
 // Qual è il metodo migliore per clonare l’oggetto restaurant, e perché? // ? Il metodo migliore è quello structureClone() perche riesco a mantenere anche gli ogetti complessi come quelli di tipo Date
+
+// ! Code question 5
+
+const hamburger = {
+  name: "Cheese Burger",
+  weight: 250,
+  maker: {
+    name: "Anonymous Chef",
+    restaurant: {
+      name: "Hyur's Burgers",
+      address: "Main Street, 123",
+      isOpen: true,
+    },
+    age: 29,
+  },
+};
+
+const newRestaurant = { ...hamburger.maker.restaurant };
+newRestaurant.name = "Hyur's II";
+newRestaurant.address = "Second Street, 12";
+const secondBurger = { ...hamburger };
+secondBurger.maker.restaurant = newRestaurant;
+secondBurger.maker.name = "Chef Hyur";
+
+console.log(hamburger.maker.name); // ? Chef Hyur
+console.log(secondBurger.maker.name); // ? Chef Hyur
+console.log(hamburger.maker.restaurant.name); // ? Hyur's II
+console.log(secondBurger.maker.restaurant.name); // ? Hyur's II
+
+// Senza lanciare il codice, riesci a prevedere cosa viene stampato in console // ?
+
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice // ? 5
+
+// ! Code question 6
+
+const chef = {
+  name: "Chef Hyur",
+  age: 29,
+  makeBurger: (num = 1) => {
+    console.log(`Ecco ${num} hamburger per te!`);
+  },
+  restaurant: {
+    name: "Hyur's Burgers",
+    welcomeClient: () => {
+      console.log("Benvenuto!");
+    },
+    address: {
+      street: "Main Street",
+      number: 123,
+      showAddress: () => {
+        console.log("Main Street 123");
+      },
+    },
+    isOpen: true,
+  },
+};
+
+// Qual è il metodo migliore per clonare l’oggetto chef, e perché? // ?
+
+// const newChef = {
+//   ...chef,
+//   restaurant: {
+//     ...chef.restaurant,
+//     address: {
+//       ...chef.restaurant.address,
+//     },
+//   },
+// };
+
+// console.log("Chef :", chef);
+// console.log("New Chef :", newChef);
+
+// 🎯 Snack (Bonus)
+// Crea una funzione che permette la copia profonda (deep copy) di un oggetto, che copia anche i suoi metodi (proprietà che contengono funzioni). Usa l’oggetto di Code Question 6 come test.
+function deepCopy(obj) {
+  if (typeof obj !== "object") {
+    return obj;
+  }
+  const copy = {};
+  for (const key in obj) {
+    const value = obj[key];
+    if (typeof value !== "object") {
+      copy[key] = value;
+    } else {
+      copy[key] = deepCopy(value);
+    }
+  }
+  return copy;
+}
+
+const chefCopy = deepCopy(chef);
+console.log(chefCopy);
+
+// ⚠️ Serve usare una funzione ricorsiva! (fai un po’ di ricerca).
